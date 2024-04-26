d79447ba4be0c936f52000ac4e1567b5a1427f59
Vagif Abilov <vagif.abilov@nrk.no>
2024-04-26 10:04:13 +0200

    Initial commit

---
+++ CSharpTests/Tests.cs
@@ -0,0 +1,136 @@
+﻿using Akka;
+using Akka.Actor;
+using Akka.Cluster;
+using Akka.Configuration;
+using Akka.Event;
+using Akka.Routing;
+using Akka.TestKit.Xunit2;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CSharpTests;
+
+public static class PoolConfig
+{
+    public static Config Get(string systemName, int seedPort, int port) => 
+        $$"""
+             akka
+             {
+                 actor
+                 {
+                     provider = "Akka.Cluster.ClusterActorRefProvider, Akka.Cluster"
+                     deployment
+                     {
+                         /echo
+                         {
+                             router = broadcast-pool
+                             cluster
+                             {
+                                 enabled = on
+                                 max-nr-of-instances-per-node = 1
+                                 max-total-nr-of-instances = 2
+                                 allow-local-routees = on
+                                 use-role = Upload
+                             }
+                         }
+                     }
+                 }
+                 
+                 remote
+                 {
+                     dot-netty.tcp
+                     {
+                         public-hostname = localhost
+                         hostname = localhost
+                         port = {{port}}
+                     }
+                 }
+                 
+                 cluster
+                 {
+                     roles = ["Upload"]
+                     seed-nodes = [ "akka.tcp://{{systemName}}@localhost:{{seedPort}}" ]
+                     min-nr-of-members = 2
+                 }
+             }
+             """;
+}
+    
+sealed class EchoActor: UntypedActor
+{
+    private readonly UniqueAddress _selfAddress;
+    private readonly ILoggingAdapter _log;
+    private readonly IActorRef _testActor;
+        
+    public EchoActor(IActorRef testActor)
+    {
+        _selfAddress = Cluster.Get(Context.System).SelfUniqueAddress;
+        _log = Context.GetLogger();
+        _testActor = testActor;
+    }
+
+    protected override void OnReceive(object message)
+    {
+        switch (message)
+        {
+            case "ping":
+                _log.Info("Received ping on {0} from {1}", _selfAddress, Sender);
+                _testActor.Tell("pong");
+                break;
+            default:
+                Unhandled(message);
+                break;
+        }
+    }
+}
+
+public class ClusterBroadcastPoolTests: TestKit
+{
+    private const string SystemName = "cluster-system-2";
+    private const int SeedPort = 5000;
+    private readonly ITestOutputHelper _out;
+    private readonly Cluster _cluster;
+    private readonly ActorSystem _system1;
+    private readonly ActorSystem _system2;
+    
+    public ClusterBroadcastPoolTests(ITestOutputHelper output) : base(PoolConfig.Get(SystemName, SeedPort, SeedPort), SystemName, output)
+    {
+        _out = output;
+        _system1 = ActorSystem.Create(SystemName, PoolConfig.Get(SystemName, SeedPort, SeedPort + 1));
+        _system2 = ActorSystem.Create(SystemName, PoolConfig.Get(SystemName, SeedPort, SeedPort + 2));
+        _cluster = Cluster.Get(_system1);
+        
+        InitializeLogger(_system1, "[SYS-1]");
+        InitializeLogger(_system2, "[SYS-2]");
+    }
+
+    [Fact]
+    public async Task ShouldBroadcastMessageToAllRoutees()
+    {
+        try
+        {
+            var tcs = new TaskCompletionSource<Done>();
+            _cluster.RegisterOnMemberUp(() => { tcs.SetResult(Done.Instance); });
+            await tcs.Task;
+
+            var probe = CreateTestProbe();
+
+            var propsWithRouter = Props.Create(() => new EchoActor(probe)).WithRouter(FromConfig.Instance);
+            var pool = Sys.ActorOf(propsWithRouter, "echo");
+
+            // wait until cluster pool stabilizes
+            await Task.Delay(1000);
+            
+            pool.Tell("ping");
+            await probe.ExpectMsgAsync("pong");
+            await probe.ExpectMsgAsync("pong");
+            await probe.ExpectNoMsgAsync(TimeSpan.FromSeconds(1));
+        }
+        finally
+        {
+            await Task.WhenAll(
+                _system1.Terminate(),
+                _system2.Terminate());
+        }
+    }
+}
---
+++ FSharpTests/Tests.fs
@@ -0,0 +1,279 @@
+﻿namespace FSharpTests
+
+module ClusterPoolTests =
+
+    open System.Threading.Tasks
+    open Akka.Routing
+    open Akka.Cluster
+    open Akka.FSharp
+    open Xunit
+    open Xunit.Abstractions
+
+    type ClusterPoolTests(output: ITestOutputHelper) =
+
+        let configWithPortJsonSerializer port =
+            Configuration.parse (
+                """
+                akka
+                {
+                    actor
+                    {
+                        provider = "Akka.Cluster.ClusterActorRefProvider, Akka.Cluster"
+                        deployment
+                        {
+                            /echo
+                            {
+                                router = broadcast-pool
+                                cluster
+                                {
+                                    enabled = on
+                                    max-nr-of-instances-per-node = 1
+                                    max-total-nr-of-instances = 2
+                                    allow-local-routees = on
+                                    use-role = worker
+                                }
+                            }
+                        }
+                    }
+                    
+                    remote
+                    {
+                        dot-netty.tcp
+                        {
+                            public-hostname = localhost
+                            hostname = localhost
+                            port = """
+                + port.ToString()
+                + """
+                        }
+                    }
+                    
+                    cluster
+                    {
+                        roles = ["worker"]
+                        seed-nodes = [ "akka.tcp://cluster-system-1@localhost:5010" ]
+                        min-nr-of-members = 2
+                    }
+                }
+            """
+            )
+
+        let configWithPortHyperionSerializer port =
+            Configuration.parse (
+                """
+                akka
+                {
+                    actor
+                    {
+                        provider = "Akka.Cluster.ClusterActorRefProvider, Akka.Cluster"
+                        serializers {
+                         hyperion = "Akka.Serialization.HyperionSerializer, Akka.Serialization.Hyperion"
+                        }
+                        serialization-bindings {
+                          "System.Object" = hyperion
+                        }
+                        serialization-identifiers {
+                          "Akka.Serialization.HyperionSerializer, Akka.Serialization.Hyperion" = -5
+                        }
+                        deployment
+                        {
+                            /echo
+                            {
+                                router = broadcast-pool
+                                cluster
+                                {
+                                    enabled = on
+                                    max-nr-of-instances-per-node = 1
+                                    max-total-nr-of-instances = 2
+                                    allow-local-routees = on
+                                    use-role = worker
+                                }
+                            }
+                        }
+                    }
+                    
+                    remote
+                    {
+                        dot-netty.tcp
+                        {
+                            public-hostname = localhost
+                            hostname = localhost
+                            port = """
+                + port.ToString()
+                + """
+                        }
+                    }
+                    
+                    cluster
+                    {
+                        roles = ["worker"]
+                        seed-nodes = [ "akka.tcp://cluster-system-2@localhost:5020" ]
+                        min-nr-of-members = 2
+                    }
+                }
+            """
+            )
+
+        [<Fact>]
+        member _.``Should broadcast message to all routees (JSON serializer)``() =
+
+            let system1 = System.create "cluster-system-1" (configWithPortJsonSerializer 5010)
+            let system2 = System.create "cluster-system-1" (configWithPortJsonSerializer 5011)
+
+            let testKit1 = new Akka.TestKit.Xunit2.TestKit(system1, output)
+            let testKit2 = new Akka.TestKit.Xunit2.TestKit(system2, output)
+
+            let echoActor (mailbox: Actor<string>) =
+                let nodeAddress = Cluster.Get(mailbox.Context.System).SelfUniqueAddress
+                let rec loop () =
+                    actor {
+                        let! msg = mailbox.Receive()
+                        match msg with
+                        | "ping" ->
+                            logDebug mailbox $"Received ping on {nodeAddress} from {mailbox.Sender().Path}"
+                            testKit1.TestActor <! "pong"
+                        | _ -> ()
+                        return! loop ()
+                    }
+                loop ()
+
+            let cluster = Cluster.Get system1
+            let tcs = TaskCompletionSource<Akka.Done>()
+            cluster.RegisterOnMemberUp(fun () -> tcs.SetResult(Akka.Done.Instance))
+            tcs.Task |> Async.AwaitTask |> Async.RunSynchronously |> ignore
+
+            let pool = spawnOpt system1 "echo" echoActor [ SpawnOption.Router(FromConfig.Instance) ]
+
+            Async.Sleep 1000 |> Async.RunSynchronously
+
+            pool <! "ping"
+            testKit1.ReceiveN 2 |> ignore
+            testKit1.ExpectNoMsg
+
+        [<Fact>]
+        member _.``Should broadcast message to all routees (Hyperion serializer)``() =
+
+            let system1 = System.create "cluster-system-2" (configWithPortHyperionSerializer 5020)
+            let system2 = System.create "cluster-system-2" (configWithPortHyperionSerializer 5021)
+
+            let testKit1 = new Akka.TestKit.Xunit2.TestKit(system1, output)
+            let testKit2 = new Akka.TestKit.Xunit2.TestKit(system2, output)
+
+            let echoActor (mailbox: Actor<string>) =
+                let nodeAddress = Cluster.Get(mailbox.Context.System).SelfUniqueAddress
+                let rec loop () =
+                    actor {
+                        let! msg = mailbox.Receive()
+                        match msg with
+                        | "ping" ->
+                            logDebug mailbox $"Received ping on {nodeAddress} from {mailbox.Sender().Path}"
+                            testKit1.TestActor <! "pong"
+                        | _ -> ()
+                        return! loop ()
+                    }
+                loop ()
+
+            let cluster = Cluster.Get system1
+            let tcs = TaskCompletionSource<Akka.Done>()
+            cluster.RegisterOnMemberUp(fun () -> tcs.SetResult(Akka.Done.Instance))
+            tcs.Task |> Async.AwaitTask |> Async.RunSynchronously |> ignore
+
+            let pool = spawnOpt system1 "echo" echoActor [ SpawnOption.Router(FromConfig.Instance) ]
+
+            Async.Sleep 1000 |> Async.RunSynchronously
+
+            pool <! "ping"
+            testKit1.ReceiveN 2 |> ignore
+            testKit1.ExpectNoMsg
+
+    type ClusterGroupTests(output: ITestOutputHelper) =
+
+        let configWithPort port =
+            Configuration.parse (
+                """
+                akka
+                {
+                    actor
+                    {
+                        provider = "Akka.Cluster.ClusterActorRefProvider, Akka.Cluster"
+                        deployment
+                        {
+                            /echo {
+                                router = broadcast-pool
+                                max-nr-of-instances = 1
+                            }
+                            /echo_group
+                            {
+                                router = broadcast-group
+                                routees.paths = ["/user/echo"]
+                                cluster
+                                {
+                                    enabled = on
+                                    max-nr-of-instances-per-node = 1
+                                    max-total-nr-of-instances = 2
+                                    allow-local-routees = on
+                                    use-role = worker
+                                }
+                            }
+                        }
+                    }
+                    
+                    remote
+                    {
+                        dot-netty.tcp
+                        {
+                            public-hostname = localhost
+                            hostname = localhost
+                            port = """
+                          + port.ToString()
+                          + """
+                        }
+                    }
+                    
+                    cluster
+                    {
+                        roles = ["worker"]
+                        seed-nodes = [ "akka.tcp://cluster-system-3@localhost:5030" ]
+                        min-nr-of-members = 2
+                    }
+                }
+            """
+            )
+
+        [<Fact>]
+        member _.``Should broadcast message to all routees``() =
+
+            let system1 = System.create "cluster-system-3" (configWithPort 5030)
+            let system2 = System.create "cluster-system-3" (configWithPort 5031)
+
+            let testKit1 = new Akka.TestKit.Xunit2.TestKit(system1, output)
+            let testKit2 = new Akka.TestKit.Xunit2.TestKit(system2, output)
+
+            let echoActor (mailbox: Actor<string>) =
+                let nodeAddress = Cluster.Get(mailbox.Context.System).SelfUniqueAddress
+                let rec loop () =
+                    actor {
+                        let! msg = mailbox.Receive()
+                        match msg with
+                        | "ping" ->
+                            logDebug mailbox $"Received ping on {nodeAddress} from {mailbox.Sender().Path}"
+                            testKit1.TestActor <! "pong"
+                        | _ -> ()
+                        return! loop ()
+                    }
+                loop ()
+
+            let cluster = Cluster.Get system1
+            let tcs = TaskCompletionSource<Akka.Done>()
+            cluster.RegisterOnMemberUp(fun () -> tcs.SetResult(Akka.Done.Instance))
+            tcs.Task |> Async.AwaitTask |> Async.RunSynchronously |> ignore
+
+            spawnOpt system1 "echo" echoActor [ SpawnOption.Router(FromConfig.Instance) ] |> ignore
+            spawnOpt system2 "echo" echoActor [ SpawnOption.Router(FromConfig.Instance) ] |> ignore
+            let group = system1.ActorOf(Akka.Actor.Props.Empty.WithRouter(FromConfig.Instance), "echo_group")
+
+            Async.Sleep 1000 |> Async.RunSynchronously
+
+            group <! "ping"
+            testKit1.ReceiveN 2 |> ignore
+            testKit1.ExpectNoMsg
